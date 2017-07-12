0e1937f0af61e0ef0cf24c217989beb4cb0995da
Sumant Bhaskaruni <bsumantb@gmail.com>
2017-07-12 16:13:10 -0400

    Update Rocket.java

--- Rocket.java
+++ Rocket.java
@@ -20,13 +20,92 @@ public class Rocket {
         }
     }
 
-    public static void body1() {
-        System.out.print("+");
+    public static void line() {
+    	System.out.print("+");
         for (int i = 0; i < 6; i++) {
             System.out.print("=*");
         }
         System.out.println("+");
-
+    }
+    
+    public static void body1() {
+    	for (int i = 0; i < 3; i++) {
+            System.out.print("|");
+            for (int j = 0; j < Math.abs(i - 2); j++) {
+            	System.out.print(".");
+            }
+            
+            for (int j = 0; j < i + 1; j++) {
+            	System.out.print("/\\");
+            }
+            
+            for (int j = 0; j < Math.abs(i - 2) * 2; j++) {
+            	System.out.print(".");
+            }
+            
+            for (int j = 0; j < i + 1; j++) {
+            	System.out.print("/\\");
+            }
+            
+            for (int j = 0; j < Math.abs(i - 2); j++) {
+            	System.out.print(".");
+            }
+            
+            System.out.println("|");
+        }
+        
+        for (int i = 0; i < 3; i++) {
+            System.out.print("|");
+            for (int j = 0; j < i; j++) {
+            	System.out.print(".");
+            }
+            
+            for (int j = 0; j < Math.abs(i - 2) + 1; j++) {
+            	System.out.print("\\/");
+            }
+            
+            for (int j = 0; j < i * 2; j++) {
+            	System.out.print(".");
+            }
+            
+            for (int j = 0; j < Math.abs(i - 2) + 1; j++) {
+            	System.out.print("\\/");
+            }
+            
+            for (int j = 0; j < i; j++) {
+            	System.out.print(".");
+            }
+            
+            System.out.println("|");
+        }
+    }
+    
+    public static void body2() {
+    	for (int i = 0; i < 3; i++) {
+            System.out.print("|");
+            for (int j = 0; j < i; j++) {
+            	System.out.print(".");
+            }
+            
+            for (int j = 0; j < Math.abs(i - 2) + 1; j++) {
+            	System.out.print("\\/");
+            }
+            
+            for (int j = 0; j < i * 2; j++) {
+            	System.out.print(".");
+            }
+            
+            for (int j = 0; j < Math.abs(i - 2) + 1; j++) {
+            	System.out.print("\\/");
+            }
+            
+            for (int j = 0; j < i; j++) {
+            	System.out.print(".");
+            }
+            
+            System.out.println("|");
+        }
+        
         for (int i = 0; i < 3; i++) {
             System.out.print("|");
             for (int j = 0; j < Math.abs(i - 2); j++) {
@@ -53,48 +132,13 @@ public class Rocket {
         }
     }
     
-    public static void body2() {
-        System.out.print("+");
-        for (int i = 0; i < 6; i++) {
-            System.out.print("=*");
-        }
-        System.out.println("+");
-
-        for (int i = 0; i < 3; i++) {
-            System.out.print("|");
-            for (int j = 0; j < i; j++) {
-            	System.out.print(".");
-            }
-            
-            for (int j = 0; j < Math.abs(i - 2) + 1; j++) {
-            	System.out.print("\\/");
-            }
-            
-            for (int j = 0; j < i * 2; j++) {
-            	System.out.print(".");
-            }
-            
-            for (int j = 0; j < Math.abs(i - 2) + 1; j++) {
-            	System.out.print("\\/");
-            }
-            
-            for (int j = 0; j < i; j++) {
-            	System.out.print(".");
-            }
-            
-            System.out.println("|");
-        }
-    }
-
     public static void main(String[] args) {
         cone();
+        line();
         body1();
+        line();
         body2();
-        System.out.print("+");
-        for (int i = 0; i < 6; i++) {
-            System.out.print("=*");
-        }
-        System.out.println("+");
+        line();
         cone();
     }
 }
