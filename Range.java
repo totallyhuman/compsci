a181bde5f368d8613aa45c6ea351f1402f620296
Sumant Bhaskaruni <bsumantb@gmail.com>
2017-07-12 17:09:05 -0400

    Add Range.java

---
+++ Range.java
@@ -0,0 +1,14 @@
+public class Range {
+    public static void printNumbers(int n) {
+        for (int i = 1; i <= n; i++) {
+            System.out.print("[" + i + "] ");
+        }
+        
+        System.out.println();
+    }
+    
+    public static void main(String[] args) {
+        printNumbers(15);
+        printNumbers(5);
+    }
+}
