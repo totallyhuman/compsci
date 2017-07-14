197edd90759fa1032b70362066053011706a65f1
Sumant Bhaskaruni <bsumantb@gmail.com>
2017-07-14 16:21:26 -0400

    Add StringMethods.java

---
+++ StringMethods.java
@@ -0,0 +1,10 @@
+public class StringMethods {
+    public static void main(String[] args) {
+        String s = "Exploring Java String Class";
+
+        System.out.println(s.charAt(7));
+        System.out.println(s.toUpperCase());
+        System.out.println(s.indexOf("Java"));
+        System.out.println(s.substring(10, 14));
+    }
+}
